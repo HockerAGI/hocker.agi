0157047a956d78bc9385065e58054960b593fe42
Hocker AGI <contacto.hocker@gmail.com>
2026-08-18 04:03:47 -0700

    fix: satisfy lint and analytics typing gates

--- src/components/analytics-consent.tsx
+++ src/components/analytics-consent.tsx
@@ -38,7 +38,9 @@ function enableOptionalMeasurement(consent: ConsentState) {
     const win = window as typeof window & { fbq?: MetaPixel };
     if (!win.fbq) {
       const queue: unknown[][] = [];
-      const fbq = ((...args: unknown[]) => queue.push(args)) as MetaPixel;
+      const fbq = ((...args: unknown[]) => {
+        queue.push(args);
+      }) as MetaPixel;
       fbq.queue = queue;
       win.fbq = fbq;
     }
